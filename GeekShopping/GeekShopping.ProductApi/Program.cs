284ee09dcdde8a18e753da2ed8f67ed53484bd32
black-adm <mattheuscontato17@gmail.com>
2025-10-08 15:01:37 -0300

    feat: database docker container and ef core migrations and context

--- GeekShopping/GeekShopping.ProductApi/Program.cs
+++ GeekShopping/GeekShopping.ProductApi/Program.cs
@@ -1,15 +1,22 @@
+using GeekShopping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connection = builder.Configuration.GetConnectionString("MySqlConnectionString") ??
+     throw new InvalidOperationException("Connection string 'MySqlConnectionString'" +
+    " not found.");
+
+builder.Services.AddDbContext<MySqlContext>(opt => 
+    opt.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 5))));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
